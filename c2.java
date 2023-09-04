b68b0df3f5c6b8a7f8d29e4e7619a8f73d0a5828
Janau00 <121666368+Janau00@users.noreply.github.com>
2023-09-04 17:10:54 +0300

    Add files via upload

---
+++ c2.java
@@ -0,0 +1,21 @@
+public class c2{
+
+ 
+public static void main (String[]args) {
+
+final double I = 200.0;
+double rantalFee = 150.5;
+int days = 9;
+
+double cost = (rantalFee*days)+ I;
+System.out.print(" total = " + cost);
+
+
+
+
+
+
+}
+
+
+}
